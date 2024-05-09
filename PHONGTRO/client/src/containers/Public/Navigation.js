2a954321c73a23a4bb92abfa30fd01c3b5de17f1
Ngocanh285 <tnanh.10a10kx2002@gmail.com>
2024-05-09 13:54:17 +0700

    Prj

---
+++ PHONGTRO/client/src/containers/Public/Navigation.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { formatVietnameseToString } from '../../ultils/Common/formatVietnameseToString'
+import { useDispatch, useSelector } from 'react-redux'
+import * as actions from '../../store/actions'
+import { path } from '../../ultils/constant'
+
+
+const notActive = 'hover:bg-secondary1 px-5 h-full flex items-center bg-[#1ba65d] gap-2'
+const active = 'hover:bg-secondary1 px-5 h-full flex items-center  bg-secondary1 gap-2'
+
+const Navigation = ({ isAdmin }) => {
+
+    const dispatch = useDispatch()
+    const { categories } = useSelector(state => state.app)
+    const { currentData } = useSelector(state => state.user)
+    useEffect(() => {
+        dispatch(actions.getCategories())
+    }, [])
+    return (
+        <div className={`w-full flex ${isAdmin ? 'justify-start' : 'justify-center'} items-center h-[40px] bg-[#1ba65d] text-white`}>
+            <div className='w-5/5 flex h-full items-center text-sm font-medium'>
+                <NavLink
+                    to={`/`}
+                    className={({ isActive }) => isActive ? active : notActive}
+                >
+                    Trang chủ
+                </NavLink>
+                {categories?.length > 0 && categories.map(item => {
+                    return (
+                        <div key={item.code} className='h-full flex justify-center items-center' >
+                            <NavLink
+                                to={`/${formatVietnameseToString(item.value)}`}
+                                className={({ isActive }) => isActive ? active : notActive}
+                            >
+                                {item.value}
+                            </NavLink>
+                        </div>
+                    )
+                })}
+                <NavLink
+                    to={path.CONTACT}
+                    className={({ isActive }) => isActive ? active : notActive}
+                >
+                    Liên hệ
+                </NavLink>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
